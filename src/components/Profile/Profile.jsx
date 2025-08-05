2124c6351e0cc672a628d27fa7ca7645cb703047
grypis <grypisnecros@gmail.com>
2025-08-05 18:03:11 +0300

    asd

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,42 +1,53 @@
 import clsx from 'clsx';
 import css from './Profile.module.css';
-import { RiRadioButtonLine } from "react-icons/ri";
-import { CiStreamOff } from "react-icons/ci";
+import { RiRadioButtonLine } from 'react-icons/ri';
+import { CiStreamOff } from 'react-icons/ci';
 
-import iconReact from "../../assets/react.svg"
-
+import iconReact from '../../assets/react.svg';
 const Profile = ({
   name,
   phone,
   email,
-  status = "offline",
-  hasPhisicalAddress
+  status = 'offline',
+  hasPhisicalAddress,
 }) => {
-console.log("css: ", css);
+  console.log('css: ', css);
 
   /* const className = ["profile"];
 
   if (hasPhisicalAddress) {
     className.push("hasPhisicalAddress");
   } */
-    return (
-      <div
-        /* className={`profile ${hasPhisicalAddress ? "hasPhisicalAddress" : ""}`}> */
-        /* className={className.join(" ")}> */
+  return (
+    <div
+      /* className={`profile ${hasPhisicalAddress ? "hasPhisicalAddress" : ""}`}> */
+      /* className={className.join(" ")}> */
       /* className={clsx("profile", hasPhisicalAddress && "hasPhisicalAddress")}> */
-        /* className={clsx("profile", {
+      /* className={clsx("profile", {
         "hasPhisicalAddress" : hasPhisicalAddress === true
       })}> */
-        className={clsx(css.profile, hasPhisicalAddress && css.hasPhisicalAddress)}>
-        <img src={iconReact} alt="" />
-        <h2 className={css.title}>Name: {name} {hasPhisicalAddress && "🏠"}</h2>
-        <p className={css.fieldRow}>Phone: {phone}</p>
-        <p className={css.fieldRow}>Email: {email}</p>
-        <p className={clsx(css.fieldRow, status === "online" && css.online, status === "offline" && css.offline)}>Status: {status === "online" ? <RiRadioButtonLine/> : <CiStreamOff/>}
-        
-        </p>
-      </div>
-    ) 
-}
-  
+      className={clsx(
+        css.profile,
+        hasPhisicalAddress && css.hasPhisicalAddress,
+      )}
+    >
+      <img src={iconReact} alt="" />
+      <h2 className={css.title}>
+        Name: {name} {hasPhisicalAddress && '🏠'}
+      </h2>
+      <p className={css.fieldRow}>Phone: {phone}</p>
+      <p className={css.fieldRow}>Email: {email}</p>
+      <p
+        className={clsx(
+          css.fieldRow,
+          status === 'online' && css.online,
+          status === 'offline' && css.offline,
+        )}
+      >
+        Status: {status === 'online' ? <RiRadioButtonLine /> : <CiStreamOff />}
+      </p>
+    </div>
+  );
+};
+
 export default Profile;
